61c758272d1bf9f3d60ec156c238825a35d42d80
Bell <bellx2@gmail.com>
2018-10-28 12:52:03 +0900

    Support pug

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,16 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-template
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
+<template lang="pug">
+  section.container
+    div
+      app-logo
+      h1.title nuxt-template
+      h2.subtitle Nuxt.js project
+      dev.links
+        a.button--green(
           href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
+          target="_blank") Documentation
+        a.button--grey(
           href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+          target="_blank") GitHub
       </div>
     </div>
   </section>
